ba2645c442e3ec8aaf2195a0b7e0c586848cfea8
goudete <goudetenrique@gmail.com>
2023-08-28 21:08:13 -0400

    mobile :

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 'use client';
 import Link from 'next/link';
 
-
 export default function Home() {
-
   return (
     <div className="flex flex-col items-center w-full max-w-5xl mx-auto min-h-screen">
-
       <header className="bg-zinc-400 w-full p-4 mt-16 mb-16 drop-shadow-2xl">
         <div className="text-lg font-semibold">Max&apos;s blog</div>
       </header>
-      <div className="w-full mb-16 flex justify-around items-center">
-        <div className="w-full mb-16 max-w-sm">
+      <div className="w-full mb-16 flex flex-col md:flex-row justify-around items-center">
+        <div className="w-full mb-4 md:mb-16 max-w-sm">
           <div className="text-lg font-semibold">
             About Me:
           </div>
@@ -32,21 +29,20 @@ export default function Home() {
             Archive
           </div>
           <div className="underline">
-            <Link href="/posts/standing-in-the-shoulders-of-giants">Standing in the shoulders of giants</Link>
+            <Link href="/posts/standing-in-the-shoulders-of-giants">Standing in the Shoulders of Giants</Link>
           </div>
           <div className="underline">
             <Link href="/posts/the-struggle">The Struggle</Link>
           </div>
         </div>
-        <div className="">
+        <div className="mb-4 md:mb-0">
           <img src="/path/to/your/image1.jpg" alt="Description of Image 1" className="w-32 h-32 object-cover" />
         </div>
-        <div className="">
+        <div className="mb-4 md:mb-0">
           <img src="/path/to/your/image1.jpg" alt="Description of Image 1" className="w-32 h-32 object-cover" />
         </div>
       </div>
     </div>
   );
 }
-
 
